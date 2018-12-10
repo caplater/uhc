5e29de7967741ed58c71230f324a0fa300a1a365
ab3189 <TcqbqB4k%4wnwd>
2018-12-09 23:19:09 -0500

    Working version

--- uhc_swift/main.swift
+++ uhc_swift/main.swift
@@ -8,5 +8,82 @@
 
 import Foundation
 
-print("Hello, World!")
+let size = 12
+
+func checkIfSafe(_ v: Int, _ graph: [[Int]], _ path: [Int], _ position: Int) -> Bool {
+    if (graph [path[position-1]][v] == 0) {
+        return false;
+    }
+    for i in 0..<position {
+        if (path[i] == v) {
+            return false
+        }
+    }
+    return true
+}
+
+func checkPosition(_ graph: [[Int]], _ path: inout [Int], _ position: Int) -> Bool {
+    
+    if (position == size) {
+        if (graph[path[position-1]][path[0]] == 1) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    for v in 1..<size {
+        if (checkIfSafe(v, graph, path, position)) {
+            path[position] = v
+            if (checkPosition(graph, &path, position+1) == true) {
+                return true;
+            }
+            path[position] = -1;
+        }
+    }
+    return false
+}
+
+func checkForHamiltonCycle(_ graph: [[Int]])->Bool {
+    
+    var path = [Int]()
+    for _ in 0..<size {
+        path.append(-1)
+    }
+    path[0] = 0
+    
+    if (checkPosition(graph, &path, 1) == false) {
+        print("No Soluition")
+        return false
+    }
+    print("Solution Found!")
+    print("A Hamiltonian Cycle:")
+    for i in 0..<size {
+        print("\(path[i]) ", terminator:"")
+    }
+    print ("\(path[0])")
+    return true
+}
+
+let graph = [
+    [0,1,1,1,1,0,0,0,1,0,0,0],// Vertex 1
+    [1,0,1,0,0,0,1,1,1,0,0,0],// Vertex 2
+    [1,1,0,0,1,1,1,0,0,0,0,0],// Vertex 3
+    [1,0,0,0,1,0,0,0,1,1,0,1],// Vertex 4
+    [1,0,1,1,0,1,0,0,0,0,0,1],// Vertex 5
+    [0,0,1,0,1,0,1,0,0,0,1,1],// Vertex 6
+    [0,1,1,0,0,1,0,1,0,0,1,0],// Vertex 7
+    [0,1,0,0,0,0,1,0,1,1,1,0],// Vertex 8
+    [1,1,0,1,0,0,0,1,0,1,0,0],// Vertex 9
+    [0,0,0,1,0,0,0,1,1,0,1,1],// Vertex 10
+    [0,0,0,0,0,1,1,1,0,1,0,1],// Vertex 11
+    [0,0,0,1,1,1,0,0,0,1,1,0] // Vertex 12
+]
+
+if (checkForHamiltonCycle(graph)) {
+    exit(0)
+}
+else {
+    exit(-1)
+}
 
