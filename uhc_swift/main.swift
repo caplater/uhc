c009f2960582f1c5fa80232e863b5a9d9236d208
ab3189 <TcqbqB4k%4wnwd>
2018-12-09 23:07:56 -0500

    Initial Commit

---
+++ uhc_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  uhc_swift
+//
+//  Created by Charles Plater on 12/9/18.
+//  Copyright © 2018 Charles Plater. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
